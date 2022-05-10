e84d9f465e15e3d01456b9f9907e4722c2e1c49f
Tom SARTORI <tom.sartori-letourneau@etu.umontpellier.fr>
2022-05-10 22:26:29 +0200

    /file command.

---
+++ src/command/Command.c
@@ -0,0 +1,64 @@
+struct Command {
+    char *name;
+    char *regex;
+    char *description;
+    /// TODO : Add action function associated.
+};
+typedef struct Command Command;
+
+/**
+ * Malloc a struct Command and set attributes.
+ *
+ * @param name
+ * @param regex
+ * @param description
+ * @return
+ */
+Command *createCommand (char *name, char *regex, char *description) {
+    Command *command = (Command *)malloc(sizeof(Command));
+
+    command->name = name;
+    command->regex = regex;
+    command->description = description;
+
+    return command;
+}
+
+Command *commandList[NB_COMMAND];
+/**
+ * Initialize the global list of available commands, commandList.
+ */
+void initCommandList () {
+    commandList[0] = createCommand("disconnect", "(^/disconnect *\n$)", "Déconnecte l'utilisateur. \n");
+    commandList[1] = createCommand("file", "(^/file +([^ ]+)\n$)", "Envoi un fichier. \n");
+    // Don't forger to update NB_COMMAND.
+}
+
+/**
+ * Return true if the message in params contains a command (starts with a "/").
+ * @param message
+ * @return
+ */
+int isCommand (char *message) {
+    return isMatch(message, "^/");
+}
+
+/**
+ * Return the Command associated to the message in params.
+ * If there is no matching command, then return NULL.
+ *
+ * @param message
+ * @return
+ */
+Command *getCommand (char *message) {
+    if (isCommand(message)) {
+        for (int i = 0; i < NB_COMMAND; i++) {
+            if (isMatch(message, commandList[i]->regex)) {
+                printf("Command : %s. \n", commandList[i]->name);
+                return commandList[i];
+            }
+        }
+    }
+    return NULL;
+}
+
---
+++ src/command/action.c
@@ -0,0 +1,97 @@
+
+#include <unistd.h>
+
+/**
+ * Shutdown the client.
+ */
+void disconnectAction () {
+    shutdownClient();
+}
+
+/**
+ * Threaded function called by fileAction(...).
+ * Connect to the socket used to send files and send the file.
+ *
+ * @param file
+ * @return
+ */
+void *fileActionThreaded (void *file) {
+    int clientSocketForFile = connectToServer(IP, PORT_SOCKET_FILE);
+
+    sendFile(clientSocketForFile, file);
+
+    close(clientSocketForFile);
+    pthread_exit(NULL);
+}
+
+/**
+ * Action used to send a file to the server.
+ * Create a thread which use fileActionThreaded(...).
+ *
+ * @param command
+ * @param message
+ */
+void fileAction (Command *command, char *message) {
+    // Get current path.
+    char filePath[200];
+    getwd(filePath);
+    if (isMatch(filePath, "cmake-build-debug")) {
+        // Program launched by Cmake project. It means that the current pwd is the directory cmake-build-debug.
+        strcat(filePath, "/../uploads/");
+    } else {
+        // Program launched by the script client.sh.
+        strcat(filePath, "/src/uploads/");
+    }
+
+    // Get filename and add it to the file path.
+    char *regexGroupList[3];
+    getRegexGroup(regexGroupList, message, command->regex);
+    strcat(filePath, regexGroupList[2]);
+
+    FILE *file;
+    file = fopen(filePath, "rb");
+    if (file != NULL) {
+        // File exists.
+        sendMessage(message);
+
+        // Create thread which send the file.
+        pthread_t fileThread;
+        pthread_create(&fileThread, NULL, fileActionThreaded, file);
+    }
+    else {
+        // File doesn't exists.
+        printf("Fichier inconnu. \n");
+    }
+
+    free(regexGroupList[0]);
+    free(regexGroupList[1]);
+    free(regexGroupList[2]);
+}
+
+/**
+ * Find the wanted command in the message in params (with regex).
+ * Call the action attached to this command.
+ * If there isn't any associated command, juste sebd the message.
+ *
+ * @param message
+ */
+void doCommandAction (char *message) {
+    Command *command = getCommand(message);
+
+    if (command == NULL) {
+        // Unknown action.
+        sendMessage(message);
+    }
+    else if (strcmp("disconnect", command->name) == 0) {
+        // Disconnection.
+        disconnectAction();
+    }
+    else if (strcmp("file", command->name) == 0) {
+        // File sending.
+        fileAction(command, message);
+    }
+    else {
+        // Unknown action.
+        sendMessage(message);
+    }
+}
